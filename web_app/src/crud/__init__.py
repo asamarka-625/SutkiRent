2cedb51e7d0f053cb7f0188037e8a5ff1744ad50
asamarka-625 <mix0x@mail.ru>
2026-02-02 15:14:21 +0300

    [backend] update content endpoints

--- web_app/src/crud/__init__.py
+++ web_app/src/crud/__init__.py
@@ -1,6 +1,6 @@
 from web_app.src.crud.apartment import (sql_get_available_apartments, sql_upd_region_for_apartments,
                                         sql_add_favorite_for_user, sql_remove_favorite_for_user)
-from web_app.src.crud.content import sql_get_contents_by_category
+from web_app.src.crud.content import sql_get_contents_by_category, sql_get_content_by_id
 from web_app.src.crud.region import sql_get_regions
 from web_app.src.crud.user import (sql_get_user_by_email, sql_get_user_by_id, sql_create_user,
                                    sql_update_password_user_by_email, sql_update_user_info)
